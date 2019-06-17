59eb240f7fde4ec183f63c3feee6b06f29d612b1
hevalhazalkurt <hevalhazal@gmail.com>
2019-06-17 15:39:06 +0300

    Deleted .py files

--- HackerRank/30_Days_of_Code_Python/Day_0_Hello_World.py
+++
@@ -1,4 +0,0 @@
-# Save a line of input from stdin to a variable, print "Hello, World."" on a single line, and finally print the value of your variable on a second line.
-input_string = input("Enter your message: ")
-print('Hello, World.')
-print(input_string)
